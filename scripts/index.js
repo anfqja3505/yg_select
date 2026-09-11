18066974a7f3450cdc4b46fdad2a70c41f35bbf5
anfqja3505 <anfqja3505@gmail.com>
2026-09-11 18:13:54 +0900

    1행 히어로 배너 완성

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
 // YG SELECT | index.js
+const menu_list_a = document.querySelectorAll('menu_list li > a')
+const search_btn = document.querySelector('#search_btn')
+const search_popup = document.querySelector('.search_popup')
+
+const heroSwiper = new Swiper('.hero_bnr',{/* 히어로배너 swiper */
+
+})
+
+search_btn.addEventListener('click',()=>{/* 검색버튼 */
+    search_popup.classList.toggle('active')
+
+})
